47339936c8b4f618ee2dbac63dd43e3a49a53bc5
Divyanshu-rajpoot <divy.r.007@gmail.com>
2023-11-21 21:27:25 +0530

    first page uploaded

---
+++ script.js
@@ -0,0 +1,29 @@
+let pointer = document.getElementById('circle')
+let generatequiz = document.getElementById('generate')
+generatequiz.addEventListener('click',()=>{
+    console.log("clicked");
+    
+})
+document.addEventListener('mousemove',function(dets){
+    let xdir = dets.x
+    let ydir = dets.y                                // genrate a follower cursor on the page
+    console.log(xdir)
+    pointer.style.transform = `translate(${xdir+5}px,${ydir+5}px)`
+})
+
+
+gsap.from( "#text h1", {
+    x:100,
+    duration: 1,
+    stagger:0.2,
+    opacity:0
+})
+gsap.from( "#btn button", {
+    y:50,
+    duration: 1,
+    stagger:0.2,
+    opacity:0
+})
+
+
+
