8efd6ff7aa7c9ef40a157b165a4da2874a4dccb9
Divyanshu-rajpoot <divy.r.007@gmail.com>
2023-11-24 09:40:01 +0530

    files name changed and extra css removed

--- question.js
+++ question.js
@@ -3,9 +3,22 @@ let optionA = document.getElementById('A');
 let optionB = document.getElementById('B');
 let optionC = document.getElementById('C');
 let optionD = document.getElementById('D');
-let correctoption = document.getElementById('correct')
+let correctoption = document.getElementById('correctopt')
 let addQuestion = document.getElementById('addques');
 let question = document.getElementById('ques');
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}                                            // this will genrate the unique id
+const quizid = generateString(4);
+idvalue.innerText = quizid;
+
 
 addQuestion.addEventListener('click', async () => {
     await fetch("https://myquizserver.onrender.com/addQuestion", {
@@ -23,21 +36,10 @@ addQuestion.addEventListener('click', async () => {
             correctOpt: `${correctoption.value}`,
         })
     })
+    const erase = document.querySelectorAll('input');
+    erase.forEach(input =>{
+        input.value = '';
+    })
 })
-function generateid() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    function generateString(length) {
-        let result = ' ';
-        const charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-
-        return result;
-    }
-    const quizid = generateString(5);
-    idvalue.innerText = quizid;
-}
-generateid();
 
 
