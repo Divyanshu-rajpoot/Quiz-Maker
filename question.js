c5f9eed5853f7cb2a7869fdc1dbfdb954a22124d
gaurav10000 <gaurav.saraswat_cs21@gla.ac.in>
2023-11-27 13:16:01 +0530

    corrected a small error in id generator

--- question.js
+++ question.js
@@ -13,7 +13,7 @@ var count = 2;
 
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 function generateString(length) {
-    let result = ' ';
+    let result = '';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
